a392c685350f5720f1762c9ad26c0ed94443379c
Thiago Franco de Moraes <totonixsame@gmail.com>
2011-11-08 23:19:34 -0200

    Loading the generated output file in the mesh class

--- taubin_ooc.cpp
+++ taubin_ooc.cpp
@@ -6,33 +6,59 @@
 #include "vec3f.hpp"
 #include "OutCorner.h"
 #include "smreader_ply.h"
+#include "smwriter_smc_cluster.h"
+#include "smreader_smc_cluster.h"
 #include "ooc_mesh.h"
 
 int main(int argc, const char *argv[])
 {
-	int event;
+	int event, NumTriForCluster;
 	FILE* arq;
+	FILE* out;
+	FILE* to_read;
+
 	SMreader_ply* smreader = new SMreader_ply;
-	COoCMesh* mesh = new COoCMesh;
+	SMwriter_smc_cluster* cluster_writer = new SMwriter_smc_cluster;
+	SMreader_smc_cluster* ooc_reader = new SMreader_smc_cluster;
+	COoCMesh * mesh = new COoCMesh;
+
+	NumTriForCluster = 200;
+
 	arq = fopen(argv[1], "r");
-	smreader->open(arq, false, false);
-	
+	smreader->open(arq, true, false);
+
+	out = fopen("/tmp/data.smc_cluster", "wb");
+	cluster_writer->open(out);
+	cluster_writer->m_OOCC.PrepareClusters(smreader->nverts, smreader->nfaces, NumTriForCluster);
+	cluster_writer->m_GraNumTriInCluster = NumTriForCluster;
+	cluster_writer->set_boundingbox(smreader->bb_min_f, smreader->bb_max_f);
+
 	while (event = smreader->read_element()) {
 		switch (event) {
 		case SM_VERTEX:
-			printf("vertex");
+			cluster_writer->write_vertex(smreader->v_pos_f);
 			break;		
 		case SM_TRIANGLE:
-			printf("triangle");
+			cluster_writer->write_triangle(smreader->t_idx, smreader->t_final);
 			break;
 		case SM_FINALIZED:
-			printf("finalized");
+			cluster_writer->write_finalized(smreader->final_idx);
+			printf("finalized\n");
 			break;
 		default:
 			break;
 		}
 	}
-	
+
+	cluster_writer->m_OOCC.Save("/tmp/meta.h");
 	smreader->close();
+	cluster_writer->close();
+
+	printf("\n\n\n\n\n");
+
+	mesh->PrepareData("/tmp", true);
+	printf("Numero de faces %d", mesh->GetNumFaces());
+	mesh->GetTri(0);
+
 	return 0;
 }
